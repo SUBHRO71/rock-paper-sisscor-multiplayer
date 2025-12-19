d30feed2e7d2cd701d4f6f39a86418176b299205
SUBHRO71 <subhro7021@gmail.com>
2025-12-19 23:54:32 +0530

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCt3EpXEuHUlLtntB71HMQp81Dvnkq_vIA",
-  authDomain: "rps-multiplayer-20bf1.firebaseapp.com",
-  projectId: "rps-multiplayer-20bf1",
-  storageBucket: "rps-multiplayer-20bf1.firebasestorage.app",
-  messagingSenderId: "824080721494",
-  appId: "1:824080721494:web:e331132c88c79fd8d8bdf7",
-  measurementId: "G-GPRTEYNSP8"
+  apiKey: "YOUR API KEY",
+  authDomain: "_",
+  projectId: "PROJECT ID",
+  storageBucket: "_",
+  messagingSenderId: "_",
+  appId: "_",
+  measurementId: "_"
 };
 
 const app = initializeApp(firebaseConfig);
